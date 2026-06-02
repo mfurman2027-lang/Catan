38fac42f59d6437de9c008efff1572f9043b487f
justin <justinanwar12@gmail.com>
2026-06-02 12:10:21 -0400

    test

--- Board.py
+++ Board.py
@@ -4,4 +4,8 @@ class Board:
     def __init__(self):
         til0 = Tile(0)
     def placePiece(self, pieceType, spot):
+    
+    def grabBoard(self):
+        return [tileList, pieceList]
         
+    def makeBoard(self):
