c8caa6311d948b5a59e2e13510bbeac0c7deadab
mfurman2027-lang <mfurman2027@student.amsacs.org>
2026-06-03 11:07:14 -0400

    board update

--- Board.py
+++ Board.py
@@ -1,21 +1,39 @@
 from Tile import Tile
 import random
+from enum import Enum
+
+
+class TileTypes(Enum):
+    FOREST = 0
+    PASTURE = 1
+    FIELD = 2
+    MOUNTAIN = 3
+    HILL = 4
+    DESERT = 5
+
 
 class Board:
     def __init__(self):
         til0 = Tile(0)
+    
     def placePiece(self, pieceType, spot):
     
     def grabBoard(self):
-        return [tileList, pieceList]
+        #return [tileList, pieceList]
+        return
         
     def makeBoard(self):
         tileList = []
-        tileValues = [4, 1, 4, 4, 3, 3]
+        
+        allTiles = [TileTypes.FOREST, TileTypes.FOREST, TileTypes.FOREST, TileTypes.FOREST, 
+                    TileTypes.PASTURE, TileTypes.PASTURE, TileTypes.PASTURE, TileTypes.PASTURE, 
+                    TileTypes.FIELD, TileTypes.FIELD, TileTypes.FIELD, TileTypes.FIELD, 
+                    TileTypes.MOUNTAIN, TileTypes.MOUNTAIN, TileTypes.MOUNTAIN, 
+                    TileTypes.DESERT]
+
         for i in range(19):
-            currTileVal = random.randint(0, 5)
-            while (tileValues[currTileVal] == 0):
-                currTileVal = random.randint(0, 5)
-            currTile = Tile(i, currTileVal, random.randint(2, 12))
-            tileValues[currTileVal] = tileValues[currTileVal] - 1
-            tileList.append(currTile)
+            randomTile = random.choice(allTiles)
+            tileList.append(randomTile)
+            allTiles.pop(allTiles.index(randomTile))
+
+        return tileList
--- Tile.py
+++ Tile.py
@@ -1,3 +1,4 @@
+from Board import TileTypes
 # brick orange 1
 # wood green 2
 # ore purple 3
@@ -6,7 +7,7 @@
 # desert tan 6
 
 # types of tiles
-# forest 4, desert 1, pasture 4, field 4, mountain 3, hill 3
+# forest 4, pasture 4, field 4, mountain 3, hill 3, desert 1
 
 class Tile:
     positionDict = {}
