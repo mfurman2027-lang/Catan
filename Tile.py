191000cff8ed02089a7dca786f59cfa9b0cb3634
mfurman2027-lang <mfurman2027@student.amsacs.org>
2026-06-03 10:45:01 -0400

    added Dict

--- Tile.py
+++ Tile.py
@@ -9,7 +9,8 @@
 # forest 4, desert 1, pasture 4, field 4, mountain 3, hill 3
 
 class Tile:
-    
+    positionDict = {}
+
     def __init__(self, pos, type):
         self.type = type
         self.image = f"CatanPictures\\Tiles\\{type}Tile.png"
