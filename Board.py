12da0ddef07b311964976024ac17f6ab52c0bc60
mfurman2027-lang <mfurman2027@student.amsacs.org>
2026-06-05 08:56:54 -0400

    added prob to Board and Tile

--- Board.py
+++ Board.py
@@ -29,14 +29,25 @@ class Board:
                     TileTypes.HILL, TileTypes.HILL, TileTypes.HILL,
                     TileTypes.DESERT]
 
+        allNums = [12, 11, 11, 10, 10, 9, 9, 8, 8, 6, 6, 5, 5, 4, 4, 3, 3, 2]
+
         for i in range(19):
             randomType = random.choice(allTiles)
-            newTile = Tile(i, randomType)
-            tileList.append(newTile)
             allTiles.pop(allTiles.index(randomType))
+            newTile = None
+
+            if randomType == TileTypes.DESERT:
+                newTile = Tile(i, randomType, 7)
+            else:
+                randomProb = random.choice(allNums)
+                allNums.pop(allNums.index(randomProb))
+
+                newTile = Tile(i, randomType, randomProb)
+            
+            tileList.append(newTile)
 
         return tileList
 
 testBoard = Board()
 for tile in testBoard.tileList:
-    print(tile.type)
+    print(f"type {tile.type} and prob {tile.prob}")
