2f7c586f94df73b254a6d3d475e65bb626f5411b
Duc-TriN <dnguyen2027@student.amsacs.org>
2026-06-10 13:42:37 -0400

    clean up

--- Catan.py
+++ Catan.py
@@ -35,25 +35,19 @@ def main():
         screen.blit(outline, (0, 0))
         for i in range(0, 3) :
             screen.blit(tileImages[i], (190 + (i * 136), 35))
-        for i in range(3, 7) :
-            screen.blit(tileImages[i], (120 + ((i - 3) * 136), 155))
-        for i in range(7, 12) :
-            screen.blit(tileImages[i], (50 + ((i - 7) * 136), 275))
-        for i in range(12, 16) :
-            screen.blit(tileImages[i], (120 + ((i - 12) * 136), 395))
-        for i in range(16, 19) :
-            screen.blit(tileImages[i], (190 + ((i - 16) * 136), 515))
-        for i in range(0, 3) :
             screen.blit(numberImages[i], (285 + (i * 136), 130))
         for i in range(3, 7) :
+            screen.blit(tileImages[i], (120 + ((i - 3) * 136), 155))
             screen.blit(numberImages[i], (215 + ((i - 3) * 136), 250))
         for i in range(7, 12) :
+            screen.blit(tileImages[i], (50 + ((i - 7) * 136), 275))
             screen.blit(numberImages[i], (145 + ((i - 7) * 136), 370))
         for i in range(12, 16) :
+            screen.blit(tileImages[i], (120 + ((i - 12) * 136), 395))
             screen.blit(numberImages[i], (215 + ((i - 12) * 136), 490))
         for i in range(16, 19) :
+            screen.blit(tileImages[i], (190 + ((i - 16) * 136), 515))
             screen.blit(numberImages[i], (285 + ((i - 16) * 136), 610))
-        # screen.blit(numberImages[0], (285, 130))
         pygame.display.flip()
 
     pygame.quit()
